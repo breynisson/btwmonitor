58e04cff2add8ad3eb6de50c0717b16fbea808ef
breynisson <bjorgvin.reynisson@gmail.com>
2017-05-13 00:42:35 +0000

    Put Team Name and current km status on page. Added a bit of styling.

--- web/channels/fetch__results.ex
+++ web/channels/fetch__results.ex
@@ -6,13 +6,22 @@ defmodule Fetch.Results do
       Hound.start_session
 
       navigate_to("http://hjoladivinnuna.is/stadan/kilometrakeppni")
-      find_element(:class, "first-place")
-      |> visible_text()
-      |> String.split(" ")
-      |> Enum.at(6)
-      |> IO.puts()
+      resvalue = find_element(:class, "first-place")
+                 |> visible_text()
+                 |> String.split(" ")
+#                 |> Enum.at(6)
+
+
+      bs = byte_size("1.\n")
+      <<_::binary-size(bs), resvalue_0::binary>> = Enum.at(resvalue, 0)
+      team_name = resvalue_0 <> " "<> Enum.at(resvalue, 1)
+
+      km = Enum.at(resvalue, 6)
 
       Hound.end_session
+
+      res = %{:name => team_name, :km_value => km}
+      res
   end
   
 end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,10 @@
 defmodule HivMonitor.PageController do
   use HivMonitor.Web, :controller
 
+  alias Fetch.Results
+
   def index(conn, _params) do
-    render conn, "index.html"
+    res = Results.fetch_firstplace_status()
+    render(conn, "index.html",firstplace_team: res[:name], firstplace_km: res[:km_value])
   end
 end
