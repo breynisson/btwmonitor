ba3d0a5e2305356e5f96e581069b4e248b820d5f
breynisson <bjorgvin.reynisson@gmail.com>
2017-05-15 12:01:03 +0000

    Added a status_email function. It is identical to the welcome_email expect for including the previous and current status in the message body.

--- lib/hiv_monitor/mailer.ex
+++ lib/hiv_monitor/mailer.ex
@@ -8,11 +8,31 @@ defmodule HivMonitor.Emails do
 
   def welcome_email do
 
-    new_email
+    new_email()
     |> to("bjorgvin.reynisson@gmail.com")
     |> from("postmaster@sandboxceff553e43ed43e88033432300ccdd75.mailgun.org")
     |> subject("Hjolad i Vinnuna: Update on 1. place km.")
     |> html_body("<strong>You can check the updated status at the hjolad i vinnuna page!</strong>")
     |> text_body("You can check the updated status at the hjolad i vinnuna page!")
+  end
+
+  def status_email(new_status, old_status) do
+
+    new_st_name = new_status[:name]
+    new_st_km = new_status[:km_value]
+
+    old_st_name = old_status[:name]
+    old_st_km = old_status[:km_value]
+
+
+    new_email()
+    |> to(["bjorgvin.reynisson@gmail.com", "b_reynisson@hotmail.com"])
+    |> from("postmaster@sandboxceff553e43ed43e88033432300ccdd75.mailgun.org")
+    |> subject("Hjolad i Vinnuna: Update on 1. place km.")
+#    |> html_body("<strong>You can check the updated status at the hjolad i vinnuna page!</strong>")
+    |> text_body("You can check the updated status at the hjolad i vinnuna page!
+
+    Old status was: #{old_st_name}, #{old_st_km}.
+    New status is: #{new_st_name}, #{new_st_km}.")
   end
 end
